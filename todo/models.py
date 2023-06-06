838fc65d43cc0fede98168836f6123f77f9e8d35
Sofia-Bourdon <mabourdonsofia@gmail.com>
2023-06-06 16:51:40 +0000

    Creating the model for todo items list

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class item(models.Model):
+    name = models.Charfield(max_lengh=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
